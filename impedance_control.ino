9c13651044aaf1a8f2eff4c85e7ffdca70ccbe1d
Artin Mokhtariha <147430167+Artinmi@users.noreply.github.com>
2025-02-22 22:21:19 +0330

    Update impedance_control.ino

--- impedance_control.ino
+++ impedance_control.ino
@@ -18,7 +18,8 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 //  PIN DEFINITIONS
 // ---------------------------
 const int buttonPin = 7; // The pin where your button is connected
-
+3
+  4
 
 // ---------------------------
 //  IMPEDANCE PARAMETERS
